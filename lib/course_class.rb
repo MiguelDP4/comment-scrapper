34519be1a8e0e200852897b19f06635d198068bf
MiguelDP4 <migueldp4@gmail.com>
2020-03-03 11:12:51 -0600

    Add class for saving course information

---
+++ lib/course_class.rb
@@ -0,0 +1,9 @@
+class Course
+  def initialize(name, lectures, hours, price, previous_price)
+    @name = name
+    @lectures = lectures
+    @hours = hours
+    @price = price
+    @previous_price = previous_price
+  end
+end
--- lib/scrap_reader.rb
+++ lib/scrap_reader.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+load 'course_class.rb'
 
 class PageReader
   def initialize(page, keyword = '', min_price = nil, max_price = nil)
