3e29c43d45f4adb89da4cf6125ad53b894da7020
MiguelDP4 <migueldp4@gmail.com>
2020-03-03 11:03:35 -0600

    Add class for reading websites

---
+++ lib/scrap_logic.rb
@@ -0,0 +1,10 @@
+require 'open-uri'
+
+class PageReader
+  def initialize(page, keyword = '', min_price = nil, max_price = nil)
+    @page = Nokogiri::HTML(open(page))
+    @keyword = keyword
+    @min_price = min_price
+    @max_price = max_price
+  end
+end
