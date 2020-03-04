34dea0d2a0abca914853f85d85a6cbf8cedc61c9
MiguelDP4 <migueldp4@gmail.com>
2020-03-04 14:58:42 -0600

    Add user interface, Correct bugs on page reader

--- lib/scrap_reader.rb
+++ lib/scrap_reader.rb
@@ -5,34 +5,35 @@ load 'lib/course_class.rb'
 
 class PageReader
   @courses = []
-    def initialize(page, keyword = '', min_price = nil, max_price = nil)
+  def initialize(page, keyword = '', min_price = nil, max_price = nil)
     @page = page
     @keyword = keyword
     @min_price = min_price
     @max_price = max_price
   end
 
-  def get_all_courses
+  def search_all_courses
     Watir.default_timeout = 120
     browser = Watir::Browser.new
     browser.goto(@page)
-    browser.element(css: ".ud-component--logged-out-home--onboarding").wait_until(&:present?)
+    browser.element(css: '.ud-component--logged-out-home--onboarding').wait_until(&:present?)
     sleep 1
-    2.times do browser.send_keys :space end
-    puts "Finding the button to view more courses"
-    arrow_number = 1
-    browser.buttons(css: ".carousel-arrow")[1].click
-    sleep 1
-    10.times do
-      browser.buttons(css: ".carousel-arrow")[2].click
-      sleep 2
+    2.times { browser.send_keys :space }
+    sleep 0.75
+    browser.buttons(css: '.carousel-arrow')[1].click
+    12.times do
+      browser.buttons(css: '.carousel-arrow')[2].click
+      sleep 0.75
     end
-    puts "Clicked the button to view more courses"
     loaded_html = Nokogiri::HTML(browser.html)
     filtered_html = loaded_html.css('.course-discovery-unit--course-discovery-unit-container--3MFr9')[1]
     slides_html = filtered_html.css('.slick-slide')
+    create_courses_array(slides_html)
+  end
+
+  def create_courses_array(slides_html)
     course_array = []
-    for slide in slides_html
+    slides_html.each do |slide|
       name = slide.css('.merchandising-course-card--course-title--2Ob4m').text
       author = slide.css('.merchandising-course-card--instructor-titles--vXVfV').text
       price = slide.css('.course-price-text').css('span span').text
@@ -43,19 +44,25 @@ class PageReader
     course_array
   end
 
-  def search_all
-
+  def search_by_name(key)
+    course_array = search_all_courses
+    course_array.select { |course| course.name.downcase.include? key.downcase }
   end
 
-  def search_by_name
-  
+  def search_by_price(min, max)
+    course_array = search_all_courses
+    course_array.select do |course|
+      course_price = course.price[1..course.price.length - 1].to_f
+      course_price >= min && course_price <= max
+    end
   end
 
-  def search_by_price
-
-  end
-
-  def search_by_all_parameters
-
+  def search_by_all_parameters(name, min, max)
+    course_array = search_all_courses
+    course_array.select do |course|
+      course_name = course.name.downcase
+      course_price = course.price[1..course.price.length - 1].to_f
+      course_name.include?(name.downcase) && course_price <= max && course_price >= min
+    end
   end
 end
--- tester.rb
+++ tester.rb
@@ -1,14 +1,15 @@
 load 'lib/scrap_reader.rb'
+require 'colorize'
 
-reader = PageReader.new("https://www.udemy.com/")
-courses = reader.get_all_courses
+reader = PageReader.new('https://www.udemy.com/')
+courses = reader.search_all_courses
 i = 1
-for course in courses
-  puts "==========================="
-  puts "Course number: #{i}"
-  puts "Course name: #{course.name}"
-  puts "Course author: #{course.author}"
-  puts "Course price: #{course.price}"
-  puts "Course original price: #{course.previous_price}"
+courses.each do |course|
+  puts '==========================='
+  puts 'Course number: '.green + i.to_s
+  puts 'Course name: '.green + course.name.to_s
+  puts 'Course author: '.green + course.author.to_s
+  puts 'Course price: '.green + course.price.to_s
+  puts 'Course original price: '.green + course.previous_price.to_s
   i += 1
 end
