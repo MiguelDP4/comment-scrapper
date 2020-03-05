88a60da8f4ae1078f6979944241fae2405c5ee5d
MiguelDP4 <migueldp4@gmail.com>
2020-03-05 13:32:15 -0600

    Fix for bug in which the site layout was different and the parser didn't recognize it

--- lib/scrap_reader.rb
+++ lib/scrap_reader.rb
@@ -41,29 +41,55 @@ class PageReader
     Watir.default_timeout = 120
     browser = Watir::Browser.new
     browser.goto(@page)
-    browser.element(css: '.ud-component--logged-out-home--onboarding').wait_until(&:present?)
-    sleep 2
-    browser.buttons(css: '.carousel-arrow')[1].click
-    12.times do
-      browser.buttons(css: '.carousel-arrow')[2].click
-      sleep 0.75
+    sleep 5
+    next_button = browser.buttons(css: '.carousel--next-button--3LJyN')
+    if next_button[7].exist?
+      13.times do
+        browser.buttons(css: '.carousel--next-button--3LJyN')[7].click
+      sleep 1
+      end
+      loaded_html = Nokogiri::HTML(browser.html)
+      filtered_html = loaded_html.css('.carousel--scroll-lock--3AYO9')[8]
+      slides_html = filtered_html.css('.carousel--scroll-item--3Wciz')
+      browser.close
+      create_courses_array(slides_html)
+    else
+      browser.buttons(css: '.carousel-arrow')[1].click
+      12.times do
+        browser.buttons(css: '.carousel-arrow')[2].click
+        sleep 0.75
+      end
+      loaded_html = Nokogiri::HTML(browser.html)
+      filtered_html = loaded_html.css('.course-discovery-unit--course-discovery-unit-container--3MFr9')[1]
+      slides_html = filtered_html.css('.slick-slide')
+      browser.close
+      create_courses_array(slides_html)
     end
-    loaded_html = Nokogiri::HTML(browser.html)
-    filtered_html = loaded_html.css('.course-discovery-unit--course-discovery-unit-container--3MFr9')[1]
-    slides_html = filtered_html.css('.slick-slide')
-    create_courses_array(slides_html)
+
   end
 
   def create_courses_array(slides_html)
     course_array = []
-    slides_html.each do |slide|
-      name = slide.css('.merchandising-course-card--course-title--2Ob4m').text
-      author = slide.css('.merchandising-course-card--instructor-titles--vXVfV').text
-      price = slide.css('.course-price-text').css('span span').text
-      previous_price = slide.css('.original-price-container').css('div span s span').text
-      tmp_course = Course.new(name, author, price, previous_price)
-      course_array.push(tmp_course) unless price == '' || previous_price == ''
+    if slides_html[0].at_css('.course-card--course-title--2f7tE')
+      slides_html.each do |slide|
+        name = slide.css('.course-card--course-title--2f7tE').text
+        author = slide.css('.course-card--instructor-list--lIA4f').text
+        price = slide.css('.course-card--discount-price--3TaBk').css('span span').text
+        previous_price = slide.css('.price-text--original-price--2e-F5').css('div span s span').text
+        tmp_course = Course.new(name, author, price, previous_price)
+        course_array.push(tmp_course) unless price == '' || previous_price == ''
+      end
+      course_array
+    else
+      slides_html.each do |slide|
+        name = slide.css('.merchandising-course-card--course-title--2Ob4m').text
+        author = slide.css('.merchandising-course-card--instructor-titles--vXVfV').text
+        price = slide.css('.course-price-text').css('span span').text
+        previous_price = slide.css('.original-price-container').css('div span s span').text
+        tmp_course = Course.new(name, author, price, previous_price)
+        course_array.push(tmp_course) unless price == '' || previous_price == ''
+      end
+      course_array
     end
-    course_array
   end
 end
